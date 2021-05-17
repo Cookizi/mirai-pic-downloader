feab9dc8fb0414568b01e1648115dbb012ad4dbe
Cookizi <tangyuefeng11@live.cn>
2021-05-18 00:00:08 +0800

    Twitter下载

--- src/main/java/top/cookizi/bot/Application.java
+++ src/main/java/top/cookizi/bot/Application.java
@@ -7,6 +7,9 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.ConfigurableApplicationContext;
 import top.cookizi.bot.config.AppConfig;
 import top.cookizi.bot.service.WebSocketService;
+import top.cookizi.bot.service.download.TwitterDownloadService;
+
+import java.io.IOException;
 
 @Slf4j
 @SpringBootApplication
--- src/main/java/top/cookizi/bot/service/download/TwitterDownloadService.java
+++ src/main/java/top/cookizi/bot/service/download/TwitterDownloadService.java
@@ -1,109 +1,27 @@
-/*
-package top.cookizi.saver.service.download;
+package top.cookizi.bot.service.download;
 
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.apache.commons.codec.digest.Md5Crypt;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import top.cookizi.saver.config.SeleniumConfig;
-import top.cookizi.saver.data.enums.DriverType;
-import top.cookizi.saver.data.msg.Msg;
-import top.cookizi.saver.data.resp.MsgResp;
+import top.cookizi.bot.config.AppConfig;
+import top.cookizi.bot.modle.msg.Msg;
+import top.cookizi.bot.modle.resp.MsgResp;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 @Service
 public class TwitterDownloadService extends AbstractDownloadService {
-
     @Autowired
-    private SeleniumConfig seleniumConfig;
-
-    */
-/**
-     * 根据传入的地址拿到图片的地址
-     *
-     * @param url 分享的地址
-     * @return 解析出来的图片地址
-     *//*
-
-    public List<String> getImgUrl(String url) {
-        System.setProperty("webdriver.gecko.driver", seleniumConfig.getPath());
-
-//        String id = getId(url);
-        RemoteWebDriver driver = DriverType.getDriver(seleniumConfig);
-
-        try {
-            driver.manage().timeouts().implicitlyWait(seleniumConfig.getTimeout(), TimeUnit.SECONDS);
-            driver.get(url);
-        } catch (Exception e) {
-            e.printStackTrace();
-            driver.quit();
-            return null;
-        }
-
-        try {
-            Thread.sleep(seleniumConfig.getTimeout() * 1000L);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-
-        WebElement element = driver.findElement(By.cssSelector("article article div[role='button']"));// /span[contains(text(), '查看')]
-        if (element != null) {
-            element.click();
-
-            try {
-                Thread.sleep(seleniumConfig.getTimeout() * 1000L);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-
-        List<WebElement> tweets = driver.findElements(By.cssSelector("article div[data-testid='tweet']"));
-        List<String> imgList = tweets.get(0).findElements(By.cssSelector("a img[src^='https://pbs.twimg.com/media/']"))
-                .stream().map(x -> x.getAttribute("src"))
-                //.filter(x -> x != null && x.startsWith("https://pbs.twimg.com/media/"))
-                .collect(Collectors.toList());
-
-        return imgList;
-
-    }
-
-   */
-/* @NotNull
-    private List<String> getImgUrls(String id) throws IOException {
-        Request request = new Request.Builder()
-                .url(TWITTER_API_URL + id)
-                .get().build();
-
-        Call call = okHttpClient.newCall(request);
-        Response response = call.execute();
-        String resp = response.body().string();
-        JsonObject jsonObject = new Gson().fromJson(resp, JsonObject.class);
-
-        List<String> imgUrlList = new ArrayList<>();
-
-        //obj.globalObjects.tweets.${id}.entities.media[].media_url
-        jsonObject.get("globalObjects").getAsJsonObject()
-                .get("tweets").getAsJsonObject()
-                .get(id).getAsJsonObject()
-                .get("entities").getAsJsonObject()
-                .get("media").getAsJsonArray()
-                .forEach(x -> imgUrlList.add(x.getAsJsonObject().get("media_url").getAsString()));
-        return imgUrlList;
-    }*//*
-
-
-    private String getId(String url) {
-        String[] split = url.split("/+|\\?");
-        return split[4];
-    }
-
+    private AppConfig appConfig;
 
     @Override
     public List<Msg> handleMsg(MsgResp msg) {
@@ -124,5 +42,67 @@ public class TwitterDownloadService extends AbstractDownloadService {
     public boolean isTypeMatch(MsgResp msg) {
         return false;
     }
+
+    class TwitterAPI {
+        String AUTH = "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA";
+        String UA = "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko";
+        Map<String, String> params = new HashMap<>();
+
+        public TwitterAPI() throws IOException {
+            String csrf = Md5Crypt.md5Crypt(String.valueOf(System.currentTimeMillis()).getBytes(StandardCharsets.UTF_8));
+            Map<String, String> heads = new HashMap<>();
+            heads.put("authorization", AUTH);
+            heads.put("x-twitter-client-language", "en");
+            heads.put("x-twitter-active-user", "yes");
+            heads.put("x-csrf-token", csrf);
+            heads.put("Origin", "https,//twitter.com");
+
+            Map<String, String> cookies = new HashMap<>();
+            cookies.put("ct0", csrf);
+            String guest_token = guest_token(heads);
+            heads.put("x-guest-token", guest_token);
+            cookies.put("gt", guest_token);
+
+            params.put("include_profile_interstitial_type", "1");
+            params.put("include_blocking", "1");
+            params.put("include_blocked_by", "1");
+            params.put("include_followed_by", "1");
+            params.put("include_want_retweets", "1");
+            params.put("include_mute_edge", "1");
+            params.put("include_can_dm", "1");
+            params.put("include_can_media_tag", "1");
+            params.put("skip_status", "1");
+            params.put("cards_platform", "Web-12");
+            params.put("include_cards", "1");
+            params.put("include_composer_source", "true");
+            params.put("include_ext_alt_text", "true");
+            params.put("include_reply_count", "1");
+            params.put("tweet_mode", "extended");
+            params.put("include_entities", "true");
+            params.put("include_user_entities", "true");
+            params.put("include_ext_media_color", "true");
+            params.put("include_ext_media_availability", "true");
+            params.put("send_error_codes", "true");
+            params.put("simple_quoted_tweet", "true");
+//        params.put(#  "count", "20");
+            params.put("count", "100");
+//        params.put(#"cursor", None);
+            params.put("ext", "mediaStats%2ChighlightedLabel%2CcameraMoment");
+            params.put("include_quote_count", "true");
+
+        }
+
+        private String guest_token(Map<String, String> heads) throws IOException {
+
+            Connection.Response response = Jsoup.connect("https://api.twitter.com/1.1/guest/activate.json")
+                    .proxy(appConfig.getProxyHost(), appConfig.getProxyPort()).headers(heads)
+                    .ignoreContentType(true).method(Connection.Method.POST)
+                    .timeout(Integer.MAX_VALUE)
+                    .execute();
+            JsonObject jsonObject = new Gson().fromJson(response.body(), JsonObject.class);
+            return jsonObject.get("guest_token").getAsString();
+        }
+
+    }
 }
-*/
+
