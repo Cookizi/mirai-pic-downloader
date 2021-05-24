cfc60b45f7f125b6c294cf917968634b0a470590
heq <522843198@qq.com>
2021-05-24 14:36:20 +0800

    feature:增加静态获取bean工具类

---
+++ src/test/java/top/cookizi/bot/GlobalTest.java
@@ -0,0 +1,16 @@
+package top.cookizi.bot;
+
+import org.junit.Test;
+
+/**
+ * @author heq
+ * @date 2021/5/24 2:34 下午
+ * @description
+ */
+public class GlobalTest {
+
+    @Test
+    public void test(){
+
+    }
+}
