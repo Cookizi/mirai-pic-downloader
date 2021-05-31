941a8bef40afa080a0af76ab7251c928ba162145
heq <522843198@qq.com>
2021-05-31 16:49:22 +0800

    feature:添加单元测试结构

--- src/test/java/top/cookizi/bot/GlobalTest.java
+++ src/test/java/top/cookizi/bot/GlobalTest.java
@@ -1,12 +1,17 @@
 package top.cookizi.bot;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * @author heq
  * @date 2021/5/24 2:34 下午
  * @description
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest()
 public class GlobalTest {
 
     @Test
